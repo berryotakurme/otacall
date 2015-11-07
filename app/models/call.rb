106c867b0b901fc1f84279d69e4fc800b5176e77
tech-camp-085 <tech-camp-085@tech-camp-085-no-MacBook-Air.local>
2015-11-07 18:21:50 +0900

    attr_accessorを設定！！

--- app/models/call.rb
+++ app/models/call.rb
@@ -2,4 +2,5 @@ class Call < ActiveRecord::Base
   belongs_to :song
   belongs_to :user
   has_many :comments, :dependent => :delete_all
+  attr_accessor :call_index
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :call
   belongs_to :user
+  attr_accessor :comment_index
 end
